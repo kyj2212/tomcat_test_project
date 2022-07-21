f54bf17c8a014a4f3f76571d00c7ef2bee9bd283
kyj2212 <kyj2212@gmail.com>
2022-07-21 13:44:20 +0900

    Fix: use rq class

--- src/main/java/com/yejin/GugudanServlet.java
+++ src/main/java/com/yejin/GugudanServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 @WebServlet("/gugudan")
@@ -14,20 +15,59 @@ public class GugudanServlet extends HttpServlet {
 
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
         //super.doGet(req, resp);
 
-        req.setCharacterEncoding("UTF-8");
+        try {
+            req.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=utf-8");
-        resp.getWriter().append("<title>구구단</title>");
 
-        int dan = Integer.parseInt(req.getParameter("dan"));
-        //System.out.println(dan+"단");
-        resp.getWriter().append("<h1>"+dan+"단</h1>");
-        resp.getWriter().append("<div>");
-        for(int i=1;i<=dan;i++)
-            resp.getWriter().append("<div>"+dan+"*"+i+"="+dan*i+"</div>");
+        Rq rq = new Rq(req,resp);
 
+        rq.appendBody("<title>구구단</title>");
+
+        int dan = rq.getIntParam("dan",9);
+        int limit = rq.getIntParam("limit",9);
+        rq.appendBody("<h1>%d단</h1>\n".formatted(dan));
+        for(int i=1;i<=limit;i++)
+         rq.appendBody("<div>"+dan+"*"+i+"="+dan*i+"</div>");
+
+    }
+}
+
+
+class Rq {
+
+    private final HttpServletRequest req;
+    private final HttpServletResponse resp;
+
+    public Rq(HttpServletRequest req, HttpServletResponse resp) {
+        this.req=req;
+        this.resp=resp;
+    }
+
+
+    public int getIntParam(String param, int defaultValue) {
+        String value = req.getParameter(param);
+
+        if(value==null)
+            return defaultValue;
+        try {
+            return Integer.parseInt(value);
+        }catch (NumberFormatException e){
+            return defaultValue;
+        }
+    }
+
+    public void appendBody(String str) {
+        try{
+            resp.getWriter().append(str);
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
     }
 }
